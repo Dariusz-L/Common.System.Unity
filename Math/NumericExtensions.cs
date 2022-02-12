fb9113061d010e2f78196eb3400cdd3c26d21c22
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-02-12 10:50:08 +0100

    Add numeric extensions class

---
+++ Math/NumericExtensions.cs
@@ -0,0 +1,16 @@
+﻿namespace Common.Domain.Collections
+{
+    public static class NumericExtensions
+    {
+        public static void IncreaseBy<T>(this ref T value, T byValue, T lowerLimit, T upperLimit) where T : struct
+        {
+            dynamic dynamicValue = value;
+
+            dynamicValue += byValue;
+            if (dynamicValue > upperLimit)
+                dynamicValue = lowerLimit;
+
+            value = dynamicValue;
+        }
+    }
+}
