8ffa1f527a2dae367c735671380de0268a09bcf6
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-02-17 09:50:30 +0100

    Add loop option to increase function

--- Math/NumericExtensions.cs
+++ Math/NumericExtensions.cs
@@ -2,14 +2,14 @@
 {
     public static class NumericExtensions
     {
-        public static void IncreaseBy<T>(this ref T value, T byValue, T lowerLimit, T upperLimit) where T : struct
+        public static void IncreaseBy<T>(this ref T value, T byValue, T lowerLimit, T upperLimit, bool loop = true) where T : struct
         {
             dynamic dynamicValue = value;
 
             dynamicValue += byValue;
             if (dynamicValue > upperLimit)
-                dynamicValue = lowerLimit;
-
+                dynamicValue = loop ? lowerLimit : upperLimit;
+                
             value = dynamicValue;
         }
     }
