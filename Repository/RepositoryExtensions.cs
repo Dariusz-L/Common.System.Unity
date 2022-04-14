3509a0da2b03348e7412ccf0d9ebf33744669910
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-04-14 16:51:50 +0200

    Add some repository extensions for shorter code

---
+++ Repository/RepositoryExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Common.Basic.Blocks;
+using System;
+using System.Threading.Tasks;
+
+namespace Common.Basic.Repository
+{
+    public static class RepositoryExtensions
+    {
+        public static Task<T> GetEntity<T>(this IRepository<T> repository, string id)
+        {
+            return Task.Run(() =>
+            {
+                var res = repository.GetBy(id).GetAwaiter().GetResult();
+                return res.Get<T>();
+            });
+        }
+
+        public static Task SaveEntity<T>(this IRepository<T> repository, T item)
+        {
+            return Task.Run(() =>
+            {
+                return repository.Save(item).GetAwaiter().GetResult();
+            });
+        }
+
+        public static async Task<Result> GetRunSaveEntity<T>(this IRepository<T> repository, string id, Func<T, bool> function)
+        {
+            var entity = await repository.GetEntity(id);
+            if (!function(entity))
+                return Result.Failure();
+
+            return repository.Save(entity);
+        }
+    }
+}
