55feb7500967cba74677feb6388726b7cc6711d2
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-04-20 13:47:10 +0200

    Add class for handling finish multiple functions

---
+++ Functional/WhenAll.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Common.System.Functional
+{
+    public class WhenAll
+    {
+        private readonly List<object> _doneResults = new List<object>();
+        private Action<object[]> _onAllDoneHandler;
+
+        public Action<object> NewHandler
+        {
+            get
+            {
+                int newIndex = _doneResults.Count;
+                _doneResults.Add(null);
+                return result =>
+                {
+                    _doneResults[newIndex] = result;
+                    Handle();
+                };
+            }
+        }
+
+        private void Handle()
+        {
+            if (!_doneResults.Any(o => o == null))
+            {
+                _onAllDoneHandler(_doneResults.ToArray());
+            }
+        }
+
+        public void OnDone<T>(Action<T[]> handler) where T : class
+        {
+            // to do: handling immediate finishes
+            _onAllDoneHandler = args =>
+            {
+                handler(args.Cast<T>().ToArray());
+            };
+        }
+    }
+}
