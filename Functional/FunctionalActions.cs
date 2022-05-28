7037f439ed3e1ba3cb4cb2f6c71741a552164a1a
dariusz.lusnia <dariusz.lusnia@gmail.com>
2022-05-28 17:39:03 +0200

    Add funtion to branch out action

--- Functional/FunctionalActions.cs
+++ Functional/FunctionalActions.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common.Basic.Collections;
+using System;
 
 namespace Common.Basic.Functional
 {
@@ -14,5 +15,8 @@ namespace Common.Basic.Functional
 
         public static void IfOkOrNot(bool value, Action onTrue, Action onFalse) =>
             IfOkOrNot(() => value, onTrue, onFalse);
+
+        public static Action<T1, T2> BranchAction<T1, T2>(params Action<T1, T2>[] actions) =>
+            (arg1, arg2) => actions.ForEach(a => a?.Invoke(arg1, arg2));
     }
 }
